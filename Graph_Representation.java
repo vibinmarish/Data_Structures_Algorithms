4ec36d514858d217bfb2349f32275e3b0efeb268
Vibin Marish <35630389+vibinmarish@users.noreply.github.com>
2020-04-22 21:43:01 +0530

    Add, Remove & Print Edges (Adjacency List)
    Program to represent a graph in java

--- Graph_Representation.java
+++ Graph_Representation.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Graph_Representation {
 	static void addEdge(ArrayList<ArrayList<Integer>> adj, int u, int v)
 	{
-		adj.get(u).add(v);		//get() will get the initial list add() will append values in the nested arraylist 
+		adj.get(u).add(v);	//get() will get the initial list add() will append values in the nested arraylist 
 		adj.get(v).add(u);
 	}
 
@@ -14,7 +14,7 @@ public class Graph_Representation {
 		for (int i = 0; i < adj.size(); i++)
 		{
 			System.out.println("\nAdjacency Vertex "+i);
-			for (int j=0; j < adj.get(i).size(); j++)		//adj.get(i).size() this will get the size of the nested arraylist
+			for (int j=0; j < adj.get(i).size(); j++)	//adj.get(i).size() this will get the size of the nested arraylist
 			{
 				System.out.print(adj.get(i).get(j)+" ");
 			}
@@ -22,7 +22,7 @@ public class Graph_Representation {
 	}
 	static void removeEdge(ArrayList<ArrayList<Integer>> adj,int u,int v)
 	{
-		adj.get(u).remove(new Integer(v));		//we use new Integer() because, to delete by value in arraylist we need object.
+		adj.get(u).remove(new Integer(v));	//we use new Integer() because, to delete by value in arraylist we need object.
 		adj.get(v).remove(new Integer(u));
 	}
 
@@ -32,7 +32,7 @@ public static void main(String args[])
 ArrayList<ArrayList<Integer>> adj=new ArrayList<ArrayList<Integer>>(vertex);	//We use arraylist within an arraylist for maintaining adjacency list
 for(int i=0;i<vertex;i++)
 {
-	adj.add(new ArrayList<Integer>());		//we did not create object for the nested arraylist above
+	adj.add(new ArrayList<Integer>());	//we did not create object for the nested arraylist above
 }
 
 addEdge(adj, 0, 1); 
