f37374818154fbd7afea967e41862fd762a26d33
Vibin Marish <35630389+vibinmarish@users.noreply.github.com>
2020-04-23 01:52:26 +0530

    Add, Remove & Print Edges (Adjacency List)
    Program to represent a graph using adjacency list

--- Graph_Representation.java
+++ Graph_Representation.java
@@ -1,51 +1,56 @@
-package Graph_Representation;
-
 import java.util.*;
 
-public class Graph_Representation {
-	static void addEdge(ArrayList<ArrayList<Integer>> adj, int u, int v)
+class Graph
+{
+	int vertex;
+	LinkedList<Integer> adj[];
+	Graph(int V)
 	{
-		adj.get(u).add(v);	//get() will get the initial list add() will append values in the nested arraylist 
-		adj.get(v).add(u);
+	vertex=V;
+	adj=new LinkedList[vertex];	//we are creating an array of objects.
+	
+	for(int i=0;i<vertex;i++)
+		adj[i]=(new LinkedList()); 
 	}
-
-	static void printGraph(ArrayList<ArrayList<Integer>> adj)
+	void addEdge(int u,int v)
 	{
-		for (int i = 0; i < adj.size(); i++)
+		adj[u].add(v);		//assigning in both because it is undirected
+		adj[v].add(u);
+	}
+	void printGraph()
+	{
+		for (int i = 0; i < vertex; i++)
 		{
-			System.out.println("\nAdjacency Vertex "+i);
-			for (int j=0; j < adj.get(i).size(); j++)	//adj.get(i).size() this will get the size of the nested arraylist
+			System.out.println("\nAdjacency Vertex: "+i);
+			for (int j=0; j < adj[i].size(); j++)		
 			{
-				System.out.print(adj.get(i).get(j)+" ");
+				System.out.print(adj[i].get(j)+" ");
 			}
 		}
 	}
-	static void removeEdge(ArrayList<ArrayList<Integer>> adj,int u,int v)
+	void removeEdge(int u,int v)
 	{
-		adj.get(u).remove(new Integer(v));	//we use new Integer() because, to delete by value in arraylist we need object.
-		adj.get(v).remove(new Integer(u));
+		adj[u].remove(new Integer(v));		//we use new Integer() because, to delete by value we need object.
+		adj[v].remove(new Integer(u));
 	}
-
-public static void main(String args[])
-{
-	int vertex=5;
-ArrayList<ArrayList<Integer>> adj=new ArrayList<ArrayList<Integer>>(vertex);	//We use arraylist within an arraylist for maintaining adjacency list
-for(int i=0;i<vertex;i++)
-{
-	adj.add(new ArrayList<Integer>());	//we did not create object for the nested arraylist above
+	
 }
-
-addEdge(adj, 0, 1); 
-addEdge(adj, 0, 4); 
-addEdge(adj, 1, 2); 
-addEdge(adj, 1, 3); 
-addEdge(adj, 1, 4); 
-addEdge(adj, 2, 3); 
-addEdge(adj, 3, 4); 
-printGraph(adj);
-removeEdge(adj,1,3);
-System.out.println("\n\nNew Graph");
-printGraph(adj);
-}
+public class Graph_Representation {
+	public static void main(String args[])
+	{
+	int v=5;
+	Graph g= new Graph(v);
+	g.addEdge(0, 1); 
+	g.addEdge(0, 4); 
+	g.addEdge(1, 2); 
+	g.addEdge(1, 3); 
+	g.addEdge(1, 4); 
+	g.addEdge(2, 3); 
+	g.addEdge(3, 4); 
+	g.printGraph();
+	g.removeEdge(1,3);
+	System.out.println("\n\nNew Graph");
+	g.printGraph();
+	}
 }
 
