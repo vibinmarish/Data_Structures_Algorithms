9f5512fab0f73e03a56d1d36d147681c1fc6b48a
Vibin Marish <35630389+vibinmarish@users.noreply.github.com>
2020-07-04 00:44:31 +0530

    Subset Sum Problem
    Given a set of non-negative integers, and a value sum, determine if there is a subset of the given set with sum equal to given sum.
    Time and Space: O(N*SUM)

--- Subset_Sum.java
+++ Subset_Sum.java
@@ -9,7 +9,7 @@ static boolean isSubsetSum(int set[],
       
       for(int i=1;i<=n;i++)
         for(int j=1;j<=sum;j++)
-          if(j-set[i-1]>=0)			//checking if the set value-sum is greater than 0
+          if(set[i-1]<=j)			//checking if the value in the set is less than or equal to sum aka j
           	matrix[i][j]=matrix[i-1][j] || matrix[i-1][j-set[i-1]];		//compare previous row value or matrix position of previous row - set value
       	  else
         	matrix[i][j]=matrix[i-1][j];		//use the previous row value
