e59b58433408f6cdb162592b59b268812d355322
Vibin Marish <35630389+vibinmarish@users.noreply.github.com>
2020-08-14 20:48:46 +0530

    O(NlogN) O(N)

--- Count_Inverse_Of_Array.java
+++ Count_Inverse_Of_Array.java
@@ -1,6 +1,6 @@
 class Inverse_Count
 {   
-static int count=0;
+
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		int a[]={2 ,4, 1, 3, 5};
@@ -8,22 +8,23 @@ static int count=0;
 		int low=0;
 		int high=n-1;
 		
-		mergeSort(a,low,high);
+		int c=mergeSort(a,low,high);
 		
-		System.out.print(count);
+		System.out.print(c);
 		
 	}
-	static void mergeSort(int a[],int low,int high)
-	{   
+	static int mergeSort(int a[],int low,int high)
+	{   int count=0;
 	    if(low<high)
 	    {
 	        int mid=low+(high-low)/2;
-	        mergeSort(a,low,mid);
-	        mergeSort(a,mid+1,high);
-	        merge(a,low,mid,high);
+	        count+=mergeSort(a,low,mid);
+	        count+=mergeSort(a,mid+1,high);
+	        count+=merge(a,low,mid,high);
 	    }
+	 return count;
 	}
-	static void merge(int a[],int low,int mid,int high)
+	static int merge(int a[],int low,int mid,int high)
 	{
 	    
 	    int m=mid-low+1;
@@ -36,7 +37,7 @@ static int count=0;
 	    
 	    for(int i=0;i<n;i++)
 	    right[i]=a[mid+i+1];
-	    int i=0,j=0,k=low;
+	    int i=0,j=0,k=low; int count=0;
 	    while(i<m && j<n)
 	    {
 	        if(left[i]<=right[j])
@@ -59,5 +60,6 @@ static int count=0;
 	    while(j<n)
 	    a[k++]=right[j++];
 	    
+		return count;
 	}
 }
