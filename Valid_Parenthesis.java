25bf6d9ff15743e7e767d1bc5acfb35e26006669
Vibin Marish <35630389+vibinmarish@users.noreply.github.com>
2020-09-18 01:53:29 +0530

    Parenthesis Checker

---
+++ Valid_Parenthesis.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        		Stack<Character> stack= new Stack<>();
+		    for(char c:s.toCharArray()){
+		        //System.out.println(stack);
+		        if(c=='{' || c=='(' || c=='['){
+		            stack.push(c);
+		        } 
+		        else if(c==')' && !stack.isEmpty() && stack.peek()=='('){
+		            stack.pop();
+		        }
+		        else if(c=='}' && !stack.isEmpty() && stack.peek()=='{'){
+		            stack.pop();
+		        }
+		        else if(c==']' && !stack.isEmpty() && stack.peek()=='['){
+		            stack.pop();
+		        }
+                		else
+                    	    return false;
+		    }
+		    
+		    
+		 return stack.isEmpty();
+    }
+}
